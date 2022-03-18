5fa3b9303109a01aa00d9d7298f11b0d78d16186
gaurav kumar <gauravkumar@gauravs-MacBook-Air.local>
2022-03-18 21:11:35 +0530

    Test1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Testing")
+import express from "express";
+const app = express();
+const port = process.env.PORT || 8001;
+
+app.listen(port,() => {
+    console.log(`Listening on ${port}`)
+})
