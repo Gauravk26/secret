85ddc60d68c1b49d197b495f20d99b12044f1567
gaurav kumar <gauravkumar@gauravs-MacBook-Air.local>
2022-03-18 20:03:48 +0530

    Memoization Fibonacci

---
+++ MemoizesFunction_Fibonacci.js
@@ -0,0 +1,17 @@
+function memoizeFib() {
+  let cache = {};
+  return function fib(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+}
+
+let fasterFib = memoizeFib();
